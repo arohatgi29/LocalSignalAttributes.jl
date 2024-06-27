0ffe9a1d8746ba91dd4f01c021eab118c7d603cc
Sergey Fomel <sergey.fomel@gmail.com>
2024-06-27 13:26:19 -0500

    extra printout

--- src/Doubleint.jl
+++ src/Doubleint.jl
@@ -2,7 +2,6 @@ function doubint!(x::Vector{T}, der=false) where T <: Real
     "causal and anticausal integration in place"
     n = length(x)  
     ranges = der ? (1:n, ) : (1:n, n:-1:1)
-    @show ranges
     for range in ranges
         t = zero(T)
         for i in range
