97b8c74585986e7ff42098cc8bfbe75f776e762c
Akshika <akshikarohatgi@utexas.edu>
2025-01-02 12:10:54 -0600

    test cases update

--- src/Ricker.jl
+++ src/Ricker.jl
@@ -12,5 +12,3 @@ function ricker(input::Array; dt=1.0, f=0.2)
     @. fourier *= freq * exp(1 - freq)
     return irfft(fourier, nt)
 end
-
-# sfricker2
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,7 @@ using Test
 
 @testset "LocalSignalAttributes.jl" begin
 
-    # Test case for ricker function
+    # [TEST CASE 1]:  Test case for ricker function
     input = [0, 0, 0, 1, 0, 0, 0]  
     dt = 0.1                        
     f = 10.0                       
@@ -12,7 +12,7 @@ using Test
     0.19267479532138973, -0.11027749899418057, 0.017917851955732096,-0.003977750622246393 ]
     @test ricker(input, dt, f) ≈ expected_output_ricker
 
-   # Test case for conjgrad function
+   # [TEST CASE 2]: Test case for conjgrad function
    forward(x) = x  
    adjoint(x) = x.^2
    shaping(x) = x  
@@ -27,14 +27,14 @@ using Test
 
    @test conjgrad(forward, adjoint, shaping, ϵ, d, p0, niter, tolerance) ≈ expected_output_conj
 
-   # Test for smooth function
+   # [TEST CASE 3]: Test for smooth function
    input_smooth = [1 2 3; 4 5 6; 7 8 9]
    nb = [1, 1]
    expected_output_smooth = [1.0 2.0 3.0; 4.0 5.0 6.0; 7.0 8.0 9.0]
    output_smooth = smooth(input_smooth, nb)
    @test output_smooth == expected_output_smooth
 
-   #Test for Smooth Division function
+   # [TEST CASE 4]: Test for Smooth Division function
    numerator = [1.0, 2.0, 3.0, 4.0]
    denominator = [0.1, 0.2, 0.3, 0.4]
    radius = [5]
@@ -43,7 +43,7 @@ using Test
    output_smooth_division = smooth_division(numerator, denominator, radius, niterations)
    @test output_smooth_division ≈ expected_output_smooth_division atol=1e-6
 
-   # Test for Hilbert
+   # [TEST CASE 5]: Test for Hilbert
    time= range(start=-0.8, step=0.004, length=10)
    signal = cos.(10*π*time .* (1 .+ 0.5*time));
    order = 200
